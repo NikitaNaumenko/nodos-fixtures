9b0c44b9ad2838c853e338ac87d5b4159af4cfa0
Naumenko Nikita <naumenkoniki@gmail.com>
2018-10-29 19:07:27 +0300

    Refactoring loadFixtures

--- src/loadFixtures.js
+++ src/loadFixtures.js
@@ -1,31 +1,21 @@
 import { Connection, Repository } from 'typeorm';
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
-import { User } from './entity/User';
 
 export default async (name, dbConnection) => {
-  let items;
   try {
     const fixturesYaml = yaml.safeLoad(fs.readFileSync(`./test/fixtures/${name}.yml`, 'utf8'));
-
-    console.log(fixturesYaml);
-    items = fixturesYaml.fixtures;
+    fixturesYaml.fixtures.forEach(async (item) => {
+      const entityName = Object.keys(item)[0];
+      const entityData = item[entityName];
+      await dbConnection
+        .createQueryBuilder()
+        .insert()
+        .into(entityName)
+        .values(entityData)
+        .execute();
+    });
   } catch (e) {
     console.log('Fixtures dont loaded, error', e);
   }
-  if (!items) {
-    return;
-  }
-  items.forEach(async (item) => {
-    const entityName = Object.keys(item)[0];
-    const entityData = item[entityName];
-    console.log(entityName);
-    console.log(entityData);
-    await dbConnection
-      .createQueryBuilder()
-      .insert()
-      .into('user')
-      .values(entityData)
-      .execute();
-  });
 };
