3eb799d5ffe6c0a5136ad7607c4741ae36bd86ca
Grigori Grudinin <gv_grudinin@bingo-boom.com>
2018-11-06 12:52:59 +0300

    add broken test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,6 +2,8 @@ import path from 'path';
 import { createConnection } from 'typeorm';
 import app from '../src/app';
 import User from '../src/entity/User';
+import Profile from '../src/entity/Profile';
+
 import baseConfig from './fixtures/config.json';
 
 const entities = [path.resolve(__dirname, '..', 'src/entity/*.js')];
@@ -18,17 +20,21 @@ describe('App test', () => {
     await app(config, path.resolve(__dirname, 'fixtures'));
 
     connection = await createConnection({ ...config, name: 'test' });
-    const data = await connection.getRepository(User).find();
+    const users = await connection.getRepository(User).find();
+    const profiles = await connection.getRepository(Profile).find();
 
-    expect(data).toHaveLength(2);
+    expect(users).toHaveLength(2);
+    expect(profiles).toHaveLength(2);
   });
 
   it('Test app with adding from fixtures2', async () => {
     await app(config, path.resolve(__dirname, 'fixtures2'));
 
     connection = await createConnection({ ...config, name: 'test' });
-    const data = await connection.getRepository(User).find();
+    const users = await connection.getRepository(User).find();
+    const profiles = await connection.getRepository(Profile).find();
 
-    expect(data).toHaveLength(3);
+    expect(users).toHaveLength(3);
+    expect(profiles).toHaveLength(2);
   });
 });
